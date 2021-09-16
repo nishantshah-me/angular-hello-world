0a400aca6d8c10e3008681b127b35a3d4d82b1e9
Nishant Shah <nishant.shah@wwindia.com>
2021-09-16 19:26:21 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    PaginationComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
