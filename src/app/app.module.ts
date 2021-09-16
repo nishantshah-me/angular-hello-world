8b8d59ee563caa505932b770526340e4e62f8c48
Nishant Shah <nishant.shah@wwindia.com>
2021-09-16 19:31:53 +0530

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -12,7 +11,6 @@ import { TableComponent } from './table/table.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
